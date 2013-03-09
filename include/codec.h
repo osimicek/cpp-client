8e13dbe1571b35e1b0944b9a4ed41855e79973b6
osimicek <ondrejsimicek@seznam.cz>
2013-03-09 17:53:42 +0100

    modifications of header files

---
+++ include/codec.h
@@ -0,0 +1,8 @@
+#ifndef DONT_INCLUDE_CODEC
+#define DONT_INCLUDE_CODEC
+
+#include "codec10.h"
+#include "codec11.h"
+#include "codec12.h"
+
+#endif
---
+++ include/consistentHash.h
@@ -0,0 +1,8 @@
+#ifndef DONT_INCLUDE_CONSISTENTHASH
+#define DONT_INCLUDE_CONSISTENTHASH
+
+#include "consistentHash10.h"
+#include "consistentHash11.h"
+#include "consistentHash12.h"
+
+#endif
