bf9b7a50197fcb11399a16f6c6b78b91b845911d
osimicek <ondrejsimicek@seznam.cz>
2013-03-16 17:35:46 +0100

    added missing files

---
+++ main.cpp
@@ -0,0 +1,453 @@
+#include <string>
+#include <remoteCache.h>
+#include <marshaller.h>
+#include <RMMap.h>
+#include <murmur/MurmurHash3.h>
+#include <murmur/MurmurHash2.h>
+
+
+
+//#include "./include/protobuf/examples/addressbook.pb.h"
+
+
+
+void print_map(std::map<std::string,std::string>  *data){
+	std::map<std::string,std::string>::iterator pos;
+	std::cout << "Bulk:" <<std::endl;
+    for (pos = (*data).begin(); pos != (*data).end(); ++pos) {
+        std::cout << "key: \"" << pos->first << "\" "<< "value: " << pos->second << std::endl;
+    }
+}
+
+// void print_rmmap(RMMap  *data){
+// 	RMMap::iterator pos;
+// 	std::cout << "RMMap:" <<std::endl;
+//     for (pos = (*data).begin(); pos != (*data).end(); ++pos) {
+//         std::cout << "key: \"" << pos->first << "\" "<< "value: " << pos->second << std::endl;
+//     }
+// }
+
+int test1(){
+	RemoteCache x = RemoteCache();
+    x.clear();
+	std::string resp;
+
+	int ret = 0;
+	
+    std::string s1 = std::string("Hell");
+    std::string s2 = std::string("aa");
+	x.putIfAbsent(&s1,&s2);
+	//std::cerr << x.get(std::string("Hell")) <<"|"<<std::endl;
+	x.get(&resp, &s1);
+
+	if( resp != s2){
+		std::cerr << "Error #1 in test1" <<resp<<std::endl;
+		ret = 1;
+	}
+	resp.clear();
+
+    std::string s3 = std::string("bb");
+	x.put(&s1,&s3);
+	x.get(&resp, &s1);
+	if(resp != s3){
+		std::cerr << "Error #2 in test1" <<std::endl;
+		ret = 1;
+	}
+	 resp.clear();
+
+    std::string s4 = std::string("cc");
+	x.replace(&s1,&s4);
+	x.get(&resp, &s1);
+	if(resp!= s4){
+		std::cerr << "Error #3 in test1" <<std::endl;
+		ret = 1;
+	}
+	resp.clear();
+    x.clear();
+    x.close();
+	return ret;
+
+}
+
+
+int test2(){
+	RemoteCache x = RemoteCache();
+    x.clear();
+	std::string resp;
+	int ret = 0;
+	int stat;
+	long long ver = 0;
+    x.put(&std::string("Hell"), &std::string("cc"));
+	x.getWithVersion(&resp, &std::string("Hell"),&ver);
+	if(resp != std::string("cc")){
+		std::cerr << "Error #1 in test2 "<< resp<<std::endl;
+		ret = 1;
+	}
+  std::cerr << "ver "<<ver<<std::endl;
+	resp.clear();
+	if((stat = x.replaceWithVersion("Hell","dd",ver++))){
+		std::cerr << "Error #2 in test2   stat:"<<stat <<std::endl;
+		ret = 1;
+	}
+	x.get(&resp, &std::string("Hell"));
+	if(resp != std::string("dd")){
+		std::cerr << "Error #3 in test2" <<std::endl;
+		ret = 1;
+	}
+	resp.clear();
+
+
+	for(int i=0;i<20;i++){
+		x.put(&std::string("Hell"),&std::string("ee"));
+	}
+	if((stat = x.replaceWithVersion(&std::string("Hell"),&std::string("dd"),ver+20))){
+		std::cerr << "Error #4 in test2   stat:"<<stat <<"  !for multiservers OK, klice se neinkrementuji"<<std::endl;
+		ret = 1;
+	}
+
+	if(stat = x.removeWithVersion(&std::string("Hell"),ver+21)){
+		std::cerr << "Error #5 in test2   stat:"<<stat <<"  !for multiservers OK, klice se neinkrementuji"<<std::endl;
+		ret = 1;
+	}
+	//x.clear();
+	std::cout <<"ping: "<<x.ping()<<"ms"<<std::endl;
+    x.clear();
+    x.close();
+	return ret;
+}
+
+int test3(){
+    std::cout <<"ok"<<std::endl;
+	RemoteCache x = RemoteCache();
+    x.clear();
+	int ret = 0;
+	int stat;
+	long long ver;
+	std::cout <<"ok"<<std::endl;
+	x.clear();
+    std::cout <<"ok"<<std::endl;
+	x.put(&std::string("Hell"),&std::string("ff"));
+	x.put(&std::string("Hell2"),&std::string("f2"));
+	x.put(&std::string("Hell3"),&std::string("f3"));
+    std::cout <<"ok"<<std::endl;
+	std::map<std::string,std::string>  bulk;
+
+	x.getBulk(&bulk);
+	print_map(&bulk);
+
+	std::map<std::string,std::string>  data;
+	data["Hell4"] = "f4";
+	data["Hell5"] = "f5";
+	data["Hell6"] = "f6";
+	x.putAll(&data);
+	bulk.clear();
+	x.getBulk(&bulk);
+	print_map(&bulk);
+
+    x.clear();
+	x.close();
+	return ret;
+}
+
+void smallTest(){
+	RemoteCache x = RemoteCache();
+	x.put(&std::string("Hell"),&std::string("hi"));
+
+/*	std::string resp;
+	x.get(&resp, &std::string("Hell"));
+	std::cout << resp << std::endl;
+
+	x.clear();
+	resp.clear();
+	x.putIfAbsent(&std::string("Hell"),&std::string("hi2"));
+	x.get(&resp, &std::string("Hell"));
+	std::cout << resp << std::endl;
+
+	resp.clear();
+	x.replace(&std::string("Hell"),&std::string("hi3"));
+	x.get(&resp, &std::string("Hell"));
+	std::cout << resp << std::endl;
+
+
+*/
+}
+
+// void loopTest(){
+// 	RMMap x;
+// 	std::string k ="a";
+// 	x[k] = "b";
+// 	print_rmmap(&x);
+
+	
+// 	RemoteCache c = RemoteCache();
+// 	while(1){
+		
+// 		c.put(&std::string("a"),&std::string("hi"));
+// 		std::string ret;
+// 		c.get(&ret,&std::string("a"));
+// 		std::cout<< ret<<std::flush<<std::endl;
+// 		c.print_servers();
+// 		//c.close();
+// 		sleep(2);
+// 	}
+
+// }
+
+void putAllTest(){
+	RemoteCache x = RemoteCache();
+	std::map<std::string,std::string>  data;
+	data["Test1"] = "Sample text #1";
+	data["Test2"] = "Sample text #2";
+	data["Test3"] = "Sample text #3";
+	data["Test4"] = "Sample text #1";
+	data["Test5"] = "Sample text #2";
+	data["Test6"] = "Sample text #3";
+	data["Test7"] = "Sample text #1";
+	data["Test8"] = "Sample text #2";
+	data["Test9"] = "Sample text #3";
+	data["Test10"] = "Sample text #1";
+	data["Test11"] = "Sample text #2";
+	data["Test12"] = "Sample text #3";
+	data["Test13"] = "Sample text #1";
+	data["Test14"] = "Sample text #2";
+	data["Test15"] = "Sample text #3";
+	data["Test12"] = "Sample text #1";
+	data["Test16"] = "Sample text #2";
+	data["Test18"] = "Sample text #3";
+	for(int i='a';i<'z';i++){
+		for(int j='a';j<'z';j++){
+
+			data[std::string("t")+(char)i+(char)j] = "bla";
+		}
+	}
+
+	struct timeval begin;
+
+    gettimeofday(&begin, NULL);
+    long long start_time,end_time, first_time, second_time;
+        
+    start_time = begin.tv_sec;
+ 
+
+	x.putAll(&data);
+
+	gettimeofday(&begin, NULL);
+	end_time = begin.tv_sec;
+    first_time = end_time - start_time;
+
+    gettimeofday(&begin, NULL);
+    start_time = begin.tv_sec;
+
+	//x.putAllAsync(&data);
+
+	gettimeofday(&begin, NULL);
+	end_time = begin.tv_sec;
+    second_time = end_time - start_time;
+
+	data.clear();
+	x.getBulk(&data);
+	print_map(&data);
+	std::cout<<"sync put all: "<<first_time<<"s \nasync put all: "<<second_time<<"s"<<std::endl;
+
+}
+
+// int MurmurHash2(void * payload) {
+//       int M = 0x5bd1e995;
+//        int R = 24;
+//       int H = -1;
+//       int h = H;
+//       int len = payload.length;
+//       int offset = 0;
+//       while (len >= 4) {
+//          int k = payload[offset];
+//          k |= payload[offset + 1] << 8;
+//          k |= payload[offset + 2] << 16;
+//          k |= payload[offset + 3] << 24;
+
+//          k *= M;
+//          k ^= k >>> R;
+//          k *= M;
+//          h *= M;
+//          h ^= k;
+
+//          len -= 4;
+//          offset += 4;
+//       }
+
+//       switch (len) {
+//          case 3:
+//             h ^= payload[offset + 2] << 16;
+//          case 2:
+//             h ^= payload[offset + 1] << 8;
+//          case 1:
+//             h ^= payload[offset];
+//             h *= M;
+//       }
+
+//       h ^= h >>> 13;
+//       h *= M;
+//       h ^= h >>> 15;
+
+//       return h;
+//    }
+
+int xx(){
+  std::cout << "test je OK" << std::endl;
+}
+#define TISK() xx()
+
+int main(){
+
+	// char key[7]; //xx
+ //    int seed = 9001;
+ //    key[0] = 3;
+ //      key[1] = 62;
+ //      key[2] = 4; //pocet
+ //      key[3] = 120;
+ //      key[4] = 120;
+ //      key[5] = 120;
+ //      key[6] = 120;
+   
+
+ //    std::string resp;
+	// RemoteCache x = RemoteCache();
+ //    std::string name(key);
+ //    x.put(&name, &name);
+ //    std::cout<< "????"<<resp<<std::flush<<std::endl;   
+ //    //x.clear();
+ //    x.put(&name, &name);
+ //    x.close();
+
+ //    uint tes;
+ //    int cis;
+ //    cis = MurmurHash3_x64_32( (const char *) key, 5,9001 );
+ //    std::cout<<std::dec<< "&&&"<<cis<<std::flush<<std::endl;  
+ //    std::cout<<std::dec<< "&&&"<<cis%2147483647<<std::flush<<std::endl;   
+ //    std::cout<<std::dec<< "&&&"<<cis%INT_MAX<<std::flush<<std::endl;   
+
+
+ //    int murmur = MurmurHash3_x64_32(key,7,9001);
+
+
+
+ //      std::cout<<std::dec<< "MUR "<<(murmur & INT_MAX) <<std::flush<<std::endl;  
+
+
+  char key[7]; //xx
+    int seed = 9001;
+    key[0] = 3;
+      key[1] = 62;
+      key[2] = 4; //pocet
+      key[3] = 120;
+      key[4] = 120;
+      key[5] = 120;
+      key[6] = 120;
+   
+
+    std::string resp;
+    RemoteCacheConfig remote_cache_config;
+    remote_cache_config.cache_name = "";
+    remote_cache_config.version = 12;
+    RemoteCache x = RemoteCache(&remote_cache_config);
+    std::string name(key);
+
+
+    std::string k("key");
+    std::string v("value");
+    // k = key;
+    x.get(&v, &k);
+    v = "value";
+
+
+    x.put(&k, &v, 1000, 1000);
+    v.clear();
+    x.get(&v, &k);
+
+
+    std::cout << "RESP " << v << std::endl;
+
+
+  //   x.ping();
+    // x.close();
+
+    // uint tes;
+    // int cis;
+    // cis = MurmurHash3_x64_32( (const char *) key, 7,9001 );
+    // std::cout<<std::dec<< "&&&"<<cis<<std::flush<<std::endl;  
+    // std::cout<<std::dec<< "&&&"<<cis%2147483647<<std::flush<<std::endl;   
+    // std::cout<<std::dec<< "&&&"<<cis%INT_MAX<<std::flush<<std::endl;   
+
+
+    // int murmur = MurmurHash3_x64_32(key,7,9001);
+    // std::cout<<std::dec<< "MUR "<<(murmur & INT_MAX) <<std::flush<<std::endl;  
+
+    // cis = MurmurHash3_x64_32( (const char *) key, 7,9001 );
+    // std::cout<<std::dec<< "&&&"<<cis<<std::flush<<std::endl;  
+    // std::cout<<std::dec<< "&&&"<<cis%2147483647<<std::flush<<std::endl;   
+    // std::cout<<std::dec<< "&&&"<<cis%INT_MAX<<std::flush<<std::endl;   
+
+
+    // int murmur2 = MurmurHash3_x64_32(key,7,9001);
+    // std::cout<<std::dec<< "MUR2 "<<(murmur2 & INT_MAX) <<std::flush<<std::endl; 
+
+
+    // std::cout<<std::dec<< test1() <<std::flush<<std::endl; 
+    // std::cout<<std::dec<< test2() <<std::flush<<std::endl; 
+    // std::cout<<std::dec<< test3() <<std::flush<<std::endl; 
+
+    // std::map<std::string,std::string>  bulk;
+
+    // x.stats(&bulk);
+    // print_map(&bulk);
+    // Metadata meta;
+    // x.getWithMetadata(&v, &meta, &k);
+    // std::cout<<std::dec<<meta.created<<std::endl;
+    // // x.get(&v,&"sfs");
+    double ff = 5.5323;
+    for(int i=0; i<8;i++){
+        std::cout<<std::hex<< (0x00ff & ((short)((char *) &ff)[i])) << " ";
+    }
+    std::cout << std::endl;
+    int dd = 9876543;
+    for(int i=0; i<4;i++){
+        std::cout<<std::hex<< (0x00ff & ((short)((char *) &dd)[i])) << " ";
+    }
+    std::cout << std::endl;
+    k = key;
+    x.get(&v, &k);
+
+    MarshallerJBoss mj;
+    int in;
+    std::string re;
+    mj.load(&re, &v);
+
+
+
+    // std::cout << in<<std::endl;
+    for(int i=0; i<10;i++){
+        std::cout<<std::hex<< (0x00ff & ((short)((char *) v.c_str())[i])) << " ";
+    }
+
+    std::cout.precision(15);
+  std::cout <<std::dec<< "double " <<re<<"   "<< re.length()<<std::endl;
+    std::string vv;
+    vv.clear();
+    int cislo = 9876543;
+    re = "nam to slape muhehehe jojojo ono to jede to je uplne super :Plkj";
+
+   std::cout <<std::dec<< "delka " <<"   "<< re.length()<<std::endl;
+    mj.dump(&vv, &re);
+    for(int i=0; i<10;i++){
+        std::cout<<std::hex<< (0x00ff & ((short)((char *) vv.c_str())[i])) << " ";
+    }
+    x.put(&k, &vv, 1000, 1000);
+	return 0;
+}
+
+
+
+    // std::vector<std::string>  keys;
+    // x.keySet(&keys);
+    // for(int t=0;t<keys.size();++t){
+    //         std::cout<<keys.at(t)<<std::endl;
+    // }
---
+++ src/remoteCache.cpp
@@ -0,0 +1,320 @@
+#include <remoteCache.h>
+
+RemoteCacheConfig::RemoteCacheConfig(){
+    version = 11;
+    lifespan = 0;
+    maxidle = 0;
+    flags = 0x00;
+    cache_name = "";
+    host = "127.0.0.1";
+    port = 11222;
+}
+
+
+RemoteCache::RemoteCache(RemoteCacheConfig* remote_cache_config){
+    init(remote_cache_config);
+}
+
+RemoteCache::RemoteCache(std::string host, int port){
+    RemoteCacheConfig remote_cache_config;
+    remote_cache_config.host = host;
+    remote_cache_config.host = port;
+    init(&remote_cache_config);
+}
+
+RemoteCache::RemoteCache(std::string host){
+    RemoteCacheConfig remote_cache_config;
+    remote_cache_config.host = host;
+    init(&remote_cache_config);
+}
+
+RemoteCache::RemoteCache(){
+    RemoteCacheConfig remote_cache_config;
+    init(&remote_cache_config);
+}
+
+void RemoteCache::init(RemoteCacheConfig* remote_cache_config){
+    srand (time(NULL));
+    transportFactory = new TransportFactory(remote_cache_config->host, remote_cache_config->port, remote_cache_config->version);
+
+    lifespan = remote_cache_config->lifespan;
+    maxidle = remote_cache_config->maxidle;
+    flags = remote_cache_config->flags;
+    cache_name = remote_cache_config->cache_name;
+
+    ping();
+}
+
+int RemoteCache::ping(){
+    PingOperation *pingOperation = new PingOperation(*transportFactory, &cache_name, flags);
+    return pingOperation->execute();
+}
+
+
+
+int RemoteCache::clear(){
+    ClearOperation *clearOperation = new ClearOperation(*transportFactory, &cache_name, flags);
+    return clearOperation->execute();
+}
+int RemoteCache::put(const char *key,const char *value,int lifespan, int maxidle){
+    std::string tmp_key(key);
+    std::string tmp_value(value);
+    return put(&tmp_key,&tmp_value,lifespan,maxidle);
+}
+
+
+int RemoteCache::put(const std::string *key,const std::string *value,int lifespan, int maxidle){
+    std::string prev_value;
+    if(lifespan < 0){lifespan = this->lifespan;}
+    if(maxidle < 0){maxidle = this->maxidle;}
+    PutOperation *putOperation = new PutOperation(value, key, &prev_value, *transportFactory, &cache_name, flags, lifespan, maxidle);
+    return putOperation->execute();
+}
+
+
+
+
+static void *print_message_function( void * t_a)
+{
+    // thread_args *t_args;
+    // t_args = (thread_args *)t_a;
+
+    // //std::cout << *t_args->key <<std::flush<<std::endl;
+    // return (void * )(t_args->RC->put(t_args->key,t_args->value,t_args->lifespan,t_args->maxidle));
+}
+
+int RemoteCache::putAllAsync(std::map<std::string,std::string> *data,int lifespan, int maxidle){
+    // int max_threads = 10;
+    // std::map<std::string,std::string>::iterator pos;
+    // int *rets = new int[data->size()];
+    // pthread_t *threads = new pthread_t[data->size()];
+    // thread_args *t_args = new thread_args[data->size()];
+    // int i =0;
+    // for (pos = (*data).begin(); pos != (*data).end(); ++pos) {
+    //     t_args[i].RC = this;
+    //     t_args[i].key = &pos->first;
+    //     t_args[i].value = &pos->second;
+    //     t_args[i].lifespan = lifespan;
+    //     t_args[i].maxidle = maxidle;
+
+    //     rets[i] = pthread_create( &threads[i], NULL, print_message_function, (void*) &t_args[i]);
+
+    //     i++;
+
+    //     if(i>= max_threads){
+    //         pthread_join( threads[i-max_threads], NULL);
+
+    //     }
+
+    // }
+    // int start = data->size() - max_threads;
+    // if(start < 0){start = 0;}
+
+    // for(int j = start;j<data->size();j++){
+    //     pthread_join( threads[j], NULL);
+    //     printf("Thread %d returns: %d\n",j,rets[j]);
+
+    // }
+    // std::cout << "done"<<std::endl;
+    return 0;
+}
+
+int RemoteCache::putAll(std::map<std::string,std::string> *data,int lifespan, int maxidle){
+    std::map<std::string,std::string>::iterator pos;
+
+    for (pos = (*data).begin(); pos != (*data).end(); ++pos) {
+       // print_servers();
+       put(&pos->first,&pos->second,lifespan,maxidle);
+    }
+
+    return 0;
+}
+
+int RemoteCache::putIfAbsent(const char *key,const char *value,int lifespan, int maxidle){
+    std::string tmp_key(key);
+    std::string tmp_value(value);
+    return putIfAbsent(&tmp_key,&tmp_value,lifespan,maxidle);
+}
+
+
+int RemoteCache::putIfAbsent(const std::string *key,std::string *value,int lifespan, int maxidle){
+    std::string prev_value;
+    if(lifespan < 0){lifespan = this->lifespan;}
+    if(maxidle < 0){maxidle = this->maxidle;}
+    PutIfAbsentOperation *putIfAbsentOperation = new PutIfAbsentOperation(value, key, &prev_value, *transportFactory, &cache_name, flags, lifespan, maxidle);
+    return putIfAbsentOperation->execute();
+}
+
+int RemoteCache::replace(const char *key,const char *value,int lifespan, int maxidle){
+    std::string tmp_key(key);
+    std::string tmp_value(value);
+    return replace(&tmp_key,&tmp_value,lifespan,maxidle);
+}
+
+
+int RemoteCache::replace(const std::string *key,std::string *value,int lifespan, int maxidle){
+    std::string prev_value;
+    if(lifespan < 0){lifespan = this->lifespan;}
+    if(maxidle < 0){maxidle = this->maxidle;}
+    ReplaceOperation *replaceOperation = new ReplaceOperation(value, key, &prev_value, *transportFactory, &cache_name, flags, lifespan, maxidle);
+    return replaceOperation->execute();
+}
+
+int RemoteCache::replaceWithVersion(const char *key,const char *value, long long version,int lifespan, int maxidle){
+    std::string tmp_key(key);
+    std::string tmp_value(value);
+    return replaceWithVersion(&tmp_key,&tmp_value, version,lifespan,maxidle);
+}
+
+
+int RemoteCache::replaceWithVersion(const std::string *key, const std::string *value, long long version,int lifespan, int maxidle){
+    /**
+    * Replaces the given value only if its version matches the supplied version.
+    *
+    * @param key key to use
+    * @param value value to use
+    * @param version numeric version that should match the one in the server
+    *                for the operation to succeed
+    * @return 0 if the value has been replaced
+    */
+    std::string prev_value;
+    if(lifespan < 0){lifespan = this->lifespan;}
+    if(maxidle < 0){maxidle = this->maxidle;}
+    ReplaceIfUnmodifiedOperation *replaceIfUnmodifiedOperation = new ReplaceIfUnmodifiedOperation(value, key, &prev_value, version, *transportFactory, &cache_name, flags, lifespan, maxidle);
+    return replaceIfUnmodifiedOperation->execute();
+}
+int RemoteCache::get(char* value, const char *key){
+    std::string tmp_key(key);
+    std::string val; //zmenit
+    int ret;
+
+    ret = get(&val,&tmp_key);
+    strcpy(value, val.c_str()); 
+    return ret;
+}
+
+int RemoteCache::get(std::string *value, const std::string *key){
+
+    GetOperation *getOperation = new GetOperation(value, key, *transportFactory, &cache_name, flags);
+    return getOperation->execute();
+    
+}
+
+int RemoteCache::remove(const char *key){
+    std::string tmp_key(key);
+    return remove(&tmp_key);
+}
+
+int RemoteCache::remove(const std::string *key){
+    std::string prev_value;
+    RemoveOperation *removeOperation = new RemoveOperation(&prev_value, key, *transportFactory, &cache_name, flags);
+    return removeOperation->execute();
+}
+
+int RemoteCache::removeWithVersion(const char *key, long long version){
+    std::string tmp_key(key);
+    return removeWithVersion(&tmp_key,version);
+}
+
+int RemoteCache::removeWithVersion(const std::string *key, long long version){
+    /**
+    * Removes the given entry only if its version matches the supplied version.
+    *
+    * @param key key to use
+    * @param version numeric version that should match the one in the server
+    *                for the operation to succeed
+    * @return 0 if the value has been removed
+    */
+    std::string prev_value;
+    RemoveIfUnmodifiedOperation *removeIfUnmodifiedOperation = new RemoveIfUnmodifiedOperation(&prev_value, key, version, *transportFactory, &cache_name, flags);
+    return removeIfUnmodifiedOperation->execute();
+    
+}
+
+int RemoteCache::containsKey(const char *key){
+    std::string tmp_key(key);
+    return containsKey(&tmp_key);
+}
+
+int RemoteCache::containsKey(const std::string *key){
+    ContainsKeyOperation *containsKeyOperation = new ContainsKeyOperation(key, *transportFactory, &cache_name, flags);
+    return containsKeyOperation->execute();
+}
+
+int RemoteCache::getWithVersion(char *value, const char *key,long long *version){
+    std::string tmp_key(key);
+    std::string val;  //zmenit
+    int ret;
+
+    ret = getWithVersion(&val,&tmp_key,version);
+    strcpy(value, val.c_str()); 
+    return ret;
+}
+
+int RemoteCache::getWithVersion(std::string *value,const std::string *key,long long *version){
+    GetWithVersionOperation *getWithVersionOperation = new GetWithVersionOperation(value, key, version, *transportFactory, &cache_name, flags);
+    return getWithVersionOperation->execute();
+}
+
+int RemoteCache::getWithMetadata(std::string *value, Metadata *meta, const std::string *key){
+    GetWithMetadataOperation *getWithMetadataOperation = new GetWithMetadataOperation(value, meta, key, *transportFactory, &cache_name, flags);
+    return getWithMetadataOperation->execute();   
+}
+
+int RemoteCache::getBulk(std::map<std::string,std::string> *bulk,int count){
+    /**
+    * Bulk get operations, returns all the entries within the remote cache.
+    *
+    * @param count maximal number of returned entries
+    * @return returns Map of string
+    */
+    GetBulkOperation *getBulkOperation = new GetBulkOperation(bulk, count, *transportFactory, &cache_name, flags);
+    return getBulkOperation->execute();  
+    
+}
+
+int RemoteCache::keySet(std::vector<std::string> *keys,int scope){
+    /**
+    * Key set operations, returns all keys in the remote server.
+    *
+    * @param scope 1 - Global scope , 2 - Local scope
+    * @return returns Vector of string
+    */
+    BulkKeysGetOperation *bulkKeysGetOperation = new BulkKeysGetOperation(keys, scope, *transportFactory, &cache_name, flags);
+    return bulkKeysGetOperation->execute();  
+    
+}
+
+int RemoteCache::getBulk(std::map<std::string,std::string> *bulk){
+    /**
+    * Bulk get operations, returns all the entries within the remote cache.
+    *
+    * @return returns Map of string
+    */
+
+    return getBulk(bulk,0);
+    
+}
+
+int RemoteCache::stats(std::map<std::string,std::string> *stats){
+    StatsOperation *statsOperation = new StatsOperation(stats, *transportFactory, &cache_name, flags);
+    return statsOperation->execute();  
+}
+
+void RemoteCache::print_servers(){
+    // std::cout << transporter->servers.size()<<std::endl; 
+    // for(int i = 0;i<transporter->servers.size();i++){
+    //     std::cout << std::dec<<transporter->servers.front().host<<"/"<<transporter->servers.front().port<<"  hash:"<<transporter->servers.front().hash<<std::endl;
+    //     transporter->servers.push(transporter->servers.front());
+    //     transporter->servers.pop();
+    // } 
+    // std::cout << transporter->servers.size()<<std::endl; 
+    // return;
+}
+
+void RemoteCache::close(){
+    // transporter->close_servers();
+
+}
+
+
